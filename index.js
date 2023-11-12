acd83226344e07bc34417607d224f837ae127427
AYUSH-PANDEY023 <145197795+AYUSH-PANDEY023@users.noreply.github.com>
2023-11-12 11:01:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Ayush.html'));
+    res.sendFile(__dirname + './Ayush.html');
 });
 
 const PORT = process.env.PORT || 3000;
